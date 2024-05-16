70635de0a06b2c5cfead55cde531bba13568f484
Viermont <viermontcode@gmail.com>
2024-05-16 17:32:58 +0200

    rename namespaces and finish M02_P03

---
+++ Modulo_02_PruebaTecnica_03/JavierMontero_M2_P3/NumberLister/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberLister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "inicio";
+            int counter = 0;
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Introduzca los números a almacenar ó escriba \"salir\": ");
+            while (input != "salir")
+            {
+                Console.Write("Número {0}: ", counter);
+                input = Console.ReadLine();
+                try
+                {
+                    if (numbers.IndexOf(Convert.ToInt32(input)) == -1 )
+                        numbers.Add(Convert.ToInt32(input));
+                    counter++;
+                }
+                catch(Exception)
+                {
+                    if (input == "salir") break;
+                    Console.WriteLine("Introduzca un valor numérico.");
+                }
+            }
+            Console.Write("Los números introducidos son: ");
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
---
+++ Modulo_02_PruebaTecnica_03/JavierMontero_M2_P3/NumberSorter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSorter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            string input;
+
+            Console.WriteLine("A continuación le solicitaré 10 números de 0 a 9:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Introduzca el número " + (i + 1) + ": ");
+                input = Console.ReadLine();
+                try
+                {
+                    if (Convert.ToInt32(input) >= 0 && Convert.ToInt32(input) < 10)
+                        numbers[i] = Convert.ToInt32(input);
+                    else
+                    {
+                        Console.WriteLine("El número debe estar entre 0 y 9.");
+                        i--;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Debe introducir un valor numérico entre 0 y 9.");
+                    i--;
+                } 
+            }
+            Array.Sort(numbers);
+            Console.Write("Los números introducidos en orden son: ");
+            foreach (var item in numbers)
+                Console.Write(numbers[item] + " ");
+        }
+    }
+}
