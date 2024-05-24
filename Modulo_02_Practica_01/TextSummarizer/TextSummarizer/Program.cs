44e3c415b84d754a60dd1825bbc41c6c0ccc04ab
Viermont <viermontcode@gmail.com>
2024-05-24 11:11:07 +0200

    I add practice 1 of module 2

---
+++ Modulo_02_Practica_01/TextSummarizer/TextSummarizer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextSummarizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int numLetters;
+
+            input = Utility.GetInputText();
+            numLetters = Utility.LetterCounter(input);
+            if (numLetters <= 35)
+            {
+                Console.WriteLine("Su texto: " + input);
+            }
+            else
+            {
+                Console.WriteLine("Su texto resumido: " + input.Substring(0, Utility.GetCutIndex(input)) + "...");
+            }
+        }
+    }
+}
---
+++ Modulo_02_Practica_01/TextSummarizer/TextSummarizer/Utility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextSummarizer
+{
+    public class Utility
+    {
+        public static string GetInputText()
+        {
+            string input;
+
+            do
+            {
+                Console.Write("A continuación introduzca un texto: ");
+                input = Console.ReadLine();
+            }
+            while (string.IsNullOrEmpty(input));
+            return input.Trim();
+        }
+        public static int LetterCounter(string input)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    counter++;
+                }
+            }
+            return counter ;
+        }
+
+        public static int GetCutIndex(string input)
+        {
+            int cutIndex = 35;
+            int numSpaces = 0;
+
+            for (int i = 0; i <= 35 + numSpaces && i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    cutIndex = i;
+                    numSpaces++;
+                }
+            }
+            return cutIndex;
+        }
+    }
+}
